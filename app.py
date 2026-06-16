008c09ab0dafe091f6e6b4071ff21b715344a410
tuyetanh1112006-creator <tuyetanh1112006@gmail.com>
2026-06-16 11:17:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_Lê Tuyết Anh")
 
 # Nhập dữ liệu
 C = st.number_input(
